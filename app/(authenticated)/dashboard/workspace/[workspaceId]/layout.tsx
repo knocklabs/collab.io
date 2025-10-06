d3d4be3021ed042f3dfae4c777359c5147ff11c7
Jeff Everhart <jeffeverhart383@gmail.com>
2025-10-06 11:27:54 -0400

    try add posthog

--- app/(authenticated)/dashboard/workspace/[workspaceId]/layout.tsx
+++ app/(authenticated)/dashboard/workspace/[workspaceId]/layout.tsx
@@ -16,6 +16,7 @@ import { AccountSwitcherWrapper } from "@/app/(authenticated)/dashboard/componen
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import { KnockNotificationProviders } from "../../providers/knock-notification-providers";
+import { PostHogProvider } from "../../providers/post-hog-provider";
 import { auth } from "@/auth";
 import { signUserToken } from "@/lib/knock";
 import { UserProfile } from "../../components/user-profile";
@@ -64,76 +65,76 @@ export default async function DashboardLayout({
   );
 
   return (
-    <KnockNotificationProviders
-      userId={session?.user?.id || ""}
-      userToken={userToken || ""}
-      workspaceId={workspaceId}
-    >
-      <SidebarProvider>
-        <div className="flex min-h-screen w-full">
-          {/* Left Sidebar */}
-          <Sidebar className="w-64 flex-shrink-0" collapsible="offcanvas">
-            <SidebarHeader>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <a
-                      href="/dashboard"
-                      className="text-xl font-semibold text-primary"
-                    >
-                      Collab.io
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-              <AccountSwitcherWrapper currentWorkspace={currentWorkspace} />
-            </SidebarHeader>
-            <SidebarContent>
-              <SidebarGroup>
-                <SidebarGroupLabel>My projects</SidebarGroupLabel>
-                <SidebarGroupContent>
-                  <SidebarMenu>
-                    {projects.length === 0 ? (
-                      <SidebarMenuItem>No projects found</SidebarMenuItem>
-                    ) : (
-                      projects.map((project) => (
-                        <SidebarMenuItem key={project.id}>
-                          <SidebarMenuButton asChild>
-                            <Link
-                              href={`/dashboard/workspace/${workspaceId}/project/${project.id}`}
-                            >
-                              {project.name}
-                            </Link>
-                          </SidebarMenuButton>
-                        </SidebarMenuItem>
-                      ))
-                    )}
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </SidebarGroup>
-              <SidebarGroup>
-                <SidebarCard />
-              </SidebarGroup>
-            </SidebarContent>
-          </Sidebar>
-          <div className="flex-1 flex flex-col h-screen">
-            <header className="h-14 border-b px-4 flex items-center justify-between w-full flex-shrink-0">
-              <div className="flex items-center space-x-4"></div>
-              <div className="flex items-center space-x-4">
-                {/* <ProjectBanner /> */}
-                <UserProfile currentUser={session?.user} />
-              </div>
-            </header>
-
-            {/* Main Content Container */}
-
-            {children}
+    <PostHogProvider>
+      <KnockNotificationProviders
+        userId={session?.user?.id || ""}
+        userToken={userToken || ""}
+        workspaceId={workspaceId}
+      >
+        <SidebarProvider>
+          <div className="flex min-h-screen w-full">
+            {/* Left Sidebar */}
+            <Sidebar className="w-64 flex-shrink-0" collapsible="offcanvas">
+              <SidebarHeader>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a
+                        href="/dashboard"
+                        className="text-xl font-semibold text-primary"
+                      >
+                        Collab.io
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+                <AccountSwitcherWrapper currentWorkspace={currentWorkspace} />
+              </SidebarHeader>
+              <SidebarContent>
+                <SidebarGroup>
+                  <SidebarGroupLabel>My projects</SidebarGroupLabel>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      {projects.length === 0 ? (
+                        <SidebarMenuItem>No projects found</SidebarMenuItem>
+                      ) : (
+                        projects.map((project) => (
+                          <SidebarMenuItem key={project.id}>
+                            <SidebarMenuButton asChild>
+                              <Link
+                                href={`/dashboard/workspace/${workspaceId}/project/${project.id}`}
+                              >
+                                {project.name}
+                              </Link>
+                            </SidebarMenuButton>
+                          </SidebarMenuItem>
+                        ))
+                      )}
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarGroup>
+                  <SidebarCard />
+                </SidebarGroup>
+              </SidebarContent>
+            </Sidebar>
+            <div className="flex-1 flex flex-col h-screen">
+              <header className="h-14 border-b px-4 flex items-center justify-between w-full flex-shrink-0">
+                <div className="flex items-center space-x-4"></div>
+                <div className="flex items-center space-x-4">
+                  {/* <ProjectBanner /> */}
+                  <UserProfile currentUser={session?.user} />
+                </div>
+              </header>
+              {/* Main Content Container */}
+              {children}
+            </div>
           </div>
-        </div>
-      </SidebarProvider>
-      {/* Modal component renders guide modals when users are eligible */}
-      <Modal />
-    </KnockNotificationProviders>
+        </SidebarProvider>
+        {/* Modal component renders guide modals when users are eligible */}
+        <Modal />
+      </KnockNotificationProviders>
+    </PostHogProvider>
   );
 }
 
